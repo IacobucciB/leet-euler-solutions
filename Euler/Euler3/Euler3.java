c30a0a853695beb0342e556c3e13c474fe25cec6
IacobucciB <link00222@gmail.com>
2024-02-23 19:01:13 -0300

    modified:   Euler/Euler3/Euler3.java

--- Euler/Euler3/Euler3.java
+++ Euler/Euler3/Euler3.java
@@ -2,8 +2,16 @@ package Euler3;
 
 public class Euler3 {
     public static void main(String args[]) {
+
         long n = 600851475143L;
-        System.out.println(n);
+        double limitD = Math.sqrt((double) n);
+        int limit = (int) limitD;
+
+    }
+
+    public static int isPrime(int x) {
+
+        return 0;
     }
 
 }
