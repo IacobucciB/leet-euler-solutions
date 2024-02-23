9daf4703153dbc7137537b03118cb064074b7e97
IacobucciB <link00222@gmail.com>
2024-02-22 22:01:23 -0300

    modified:   Euler2/Euler2.java 	new file:   Euler3/Euler3.java

--- Euler2/Euler2.java
+++ Euler2/Euler2.java
@@ -3,19 +3,26 @@ package Euler2;
 public class Euler2 {
 
     public static void main(String args[]) {
-    
-        int limit = 250;
-    
-        int n1 = 0;
-        int n2 = 1;
-        int fib = 0;
-        while(fib < limit) {
-            fib = n1 + n2;
-            n1 = n2;
-            n2 = fib;
-            
 
-            System.out.println(fib);
-        } 
+        int limit = 4000000;
+        int sum = 0;
+
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 0;
+
+        do {
+
+            if (fn % 2 == 0) {
+                sum = sum + fn;
+            }
+
+            fn = f0 + f1;
+            f0 = f1;
+            f1 = fn;
+
+        } while (fn < limit);
+
+        System.out.println(sum);
     }
 }
---
+++ Euler3/Euler3.java
@@ -0,0 +1,9 @@
+package Euler3;
+
+public class Euler3 {
+    public static void main(String args[]) {
+        long n = 600851475143L;
+        System.out.println(n);
+    }
+
+}
