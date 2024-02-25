74aefb0c7dcb79ef8d53322dbd90f17b0c763f89
IacobucciB <link00222@gmail.com>
2024-02-25 16:19:50 -0300

    modified:   Euler/Euler3/Euler3.java

--- Euler/Euler3/Euler3.java
+++ Euler/Euler3/Euler3.java
@@ -7,11 +7,31 @@ public class Euler3 {
         double limitD = Math.sqrt((double) n);
         int limit = (int) limitD;
 
+        for (int i = 0; i <= limit; i++) {
+            if (isPrime(i)) {
+                if (limit % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 
-    public static int isPrime(int x) {
-
-        return 0;
+    public static boolean isPrime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        if (x <= 3) {
+            return true;
+        }
+        if (x % 2 == 0 || x % 3 == 0) {
+            return false;
+        }
+        // Check divisibility by xs of the form 6k ± 1 up to sqrt(x)
+        for (int i = 5; i * i <= x; i += 6) {
+            if (x % i == 0 || x % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-
 }
